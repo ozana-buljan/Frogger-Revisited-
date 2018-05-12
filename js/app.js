65f8ea162764a1b093e86d3d1e23ad915fbf61c1
Ozana Buljan <ozana.buljan@gmail.com>
2018-05-12 19:39:30 +0200

    JS - added Enemy & Player constructor

--- js/app.js
+++ js/app.js
@@ -35,14 +35,13 @@ class Game {
 }
 
 // Enemies our player must avoid
-class Enemy {
-    constructor() {
-        // Variables applied to each of our instances go here, we've provided one for you to get started
-
-        // The image/sprite for our enemies, this uses a helper we've provided to easily load images
-        this.sprite = "images/enemy-bug.png";
-
+class Enemy extends Game {
+    constructor(x, y, image, speed) {
+        super(x, y, image, speed);
+        this.speed = speed;
+        this.sprite = "images/enemy-bug.png"; // The image/sprite for enemies
     }
+
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
@@ -51,16 +50,15 @@ class Enemy {
         // all computers.
     }
     // Draw the enemy on the screen, required method for game
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
 }
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-    constructor() {
-
+    constructor(x, y, image, lives) {
+        super(x, y, image, lives);
+        this.lives = lives;
+        this.sprite = "images/char-boy.png";
     }
     update(dt) {
 
